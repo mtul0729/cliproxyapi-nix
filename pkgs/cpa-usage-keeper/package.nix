b0695e4904ca158533512f9a9e8949bfdffac6ef
mtul0729 <hdx0729@gmail.com>
2026-06-23 04:17:44 +0800

    [create-pull-request] automated change (#55)

--- pkgs/cpa-usage-keeper/package.nix
+++ pkgs/cpa-usage-keeper/package.nix
@@ -7,11 +7,11 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "cpa-usage-keeper";
-  version = "1.11.2";
+  version = "1.11.3";
 
   src = fetchurl {
     url = "https://github.com/Willxup/cpa-usage-keeper/releases/download/v${finalAttrs.version}/cpa-usage-keeper_v${finalAttrs.version}_linux_amd64.tar.gz";
-    hash = "sha256-Mekv0Kj48j6dw5cvXxE77dmYdI5aGhh4D0hVlZUxGx0=";
+    hash = "sha256-2w2kLkg3uQ0QmlXzxhMxk9ifsi3sz9Im6OvF9/SP2vc=";
   };
 
   nativeBuildInputs = [ autoPatchelfHook ];
