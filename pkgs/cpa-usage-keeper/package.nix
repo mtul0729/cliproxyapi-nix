427edcee3c348b013c6c0dfa982490963844b8b1
mtul0729 <52401682+mtul0729@users.noreply.github.com>
2026-07-02 19:51:22 +0000

    [create-pull-request] automated change

--- pkgs/cpa-usage-keeper/package.nix
+++ pkgs/cpa-usage-keeper/package.nix
@@ -7,11 +7,11 @@
 }:
 stdenv.mkDerivation (finalAttrs: {
   pname = "cpa-usage-keeper";
-  version = "1.12.3";
+  version = "1.12.4";
 
   src = fetchurl {
     url = "https://github.com/Willxup/cpa-usage-keeper/releases/download/v${finalAttrs.version}/cpa-usage-keeper_v${finalAttrs.version}_linux_amd64.tar.gz";
-    hash = "sha256-XZbRv+/g/R0zKUbuglEbJu9fD9iycB1qZEPD9le8Un8=";
+    hash = "sha256-kt24jmvd/VKZ1500OdRx7thwS4xi3os66zKEPo8tbX8=";
   };
 
   nativeBuildInputs = [ autoPatchelfHook ];
