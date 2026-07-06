2fb9cd44e108dead60a2af416b5a7768081c9147
mtul0729 <52401682+mtul0729@users.noreply.github.com>
2026-07-06 21:03:25 +0000

    [create-pull-request] automated change

--- pkgs/cliproxyapi/package.nix
+++ pkgs/cliproxyapi/package.nix
@@ -6,13 +6,13 @@
 }:
 buildGoModule rec {
   pname = "cliproxyapi";
-  version = "7.2.50";
+  version = "7.2.51";
 
   src = fetchFromGitHub {
     owner = "router-for-me";
     repo = "CLIProxyAPI";
     rev = "v${version}";
-    hash = "sha256-MSKLk+vAVOBSvZpxalgE7hL2vOzBb7IsR4Wqt1QPYGY=";
+    hash = "sha256-pDNK8vz8CbLWOIZYXoEEXxlTFcmNKuRyRJlH4gDkceU=";
   };
 
   vendorHash = "sha256-vQU3hLDga5PMUwH4KSB3T5sZ1uPUgHQHeyQGJTKHIYs=";
